b19f5da5a8f661de77b5a5f04f8d294ad76f8e2d
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2023-10-22 09:46:15 -0300

    inclusão de texto em imagem goku-dedo

--- exercicio3.js
+++
@@ -1,9 +0,0 @@
-const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];{
-    //console.log(numbers);
-       for (let i = 0; i < numbers.length; i++) {
-        const soma = 0;
-        var soma = i + i;
-        console.log(soma);
-        
-    }
-}
--- teste.js
+++
@@ -1,4 +0,0 @@
-let numero1 = 4
-let numero2 = 9
-console.log("wesley" + " soares" + 1)
-

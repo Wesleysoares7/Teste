c16ee551ba3fe2aaa45f5e2ecd3e619e61886f66
Wesleysoares7 <wesleysoares7@yahoo.com.br>
2023-11-14 20:35:09 -0300

    Criamos cards utilizando javascript

--- script.js
+++ script.js
@@ -1,3 +1,57 @@
-const cards = document.querySelectorAll('.card')
-const vegeta = cards[1]
-vegeta.style.background="red"
+const header = document.querySelector('header')
+header.style.background="blue"
+
+const titulo = document.createElement('h1')
+titulo.innerText='dragon ball z'
+titulo.style.marginTop="6rem"
+const tituloConteiner = document.querySelector('#titulo')
+tituloConteiner.appendChild(titulo)
+
+const personagens = [
+    {
+        nome:"Goku",
+        img:"./imagens/goku saga z.webp",
+        link:"https://pt.wikipedia.org/wiki/Goku"
+        
+        },
+        {
+            nome:"Vegeta",
+            img:"./imagens/vegeta saga z.webp",
+            link:"https://pt.wikipedia.org/wiki/Vegeta"
+
+        },
+        {
+            nome:"Gohan",
+            img:"./imagens/Gohan.webp",
+            link:"https://pt.wikipedia.org/wiki/Son_Gohan"
+        },
+        {
+            nome:"Kuririn",
+            img:"./imagens/kuririn saga z.webp",
+            link:"https://pt.wikipedia.org/wiki/Kuririn"
+        }
+]
+
+
+for (let index = 0; index < personagens.length; index++) {
+    const personagem = personagens[index];
+    const card = document.createElement('div')
+    card.className='card'
+    const linkImg = document.createElement('a')
+    linkImg.href=personagem.link
+    linkImg.target='_blank'
+    const linkTitulo = document.createElement('a')
+    linkTitulo.href=personagem.link
+    linkTitulo.target='_blank'
+    const cardTitulo = document.createElement('p')
+    cardTitulo.innerText=personagem.nome
+    const cardImg = document.createElement('img')
+    cardImg.src=personagem.img
+    linkTitulo.appendChild(cardTitulo)
+    linkImg.appendChild(cardImg)
+    card.appendChild(linkTitulo)
+    card.appendChild(linkImg)
+    const cardConteiner = document.querySelector('#cards-conteiner')
+    cardConteiner.appendChild(card)
+
+}
